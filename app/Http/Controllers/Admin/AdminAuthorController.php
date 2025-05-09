04ae7724276f3529a6dabfdbbd4edea7b07bc6a9
Hanif-tech <hanif.rifqi17@gmail.com>
2025-05-09 10:54:42 +0700

    update fix cms error

--- app/Http/Controllers/Admin/AdminAuthorController.php
+++ app/Http/Controllers/Admin/AdminAuthorController.php
@@ -27,25 +27,25 @@ class AdminAuthorController extends Controller
     {
         $author = new Author();
 
-        $request->validate([         
+        $request->validate([
             'name' => 'required',
             'email' => 'required|email|unique:authors',
             'password' => 'required',
             'retype_password' => 'required|same:password'
         ]);
 
-        if($request->hasFile('photo')) {
-            $request->validate([         
+        if ($request->hasFile('photo')) {
+            $request->validate([
                 'photo' => 'image|mimes:jpg,jpeg,png,gif'
             ]);
 
             $now = time();
             $ext = $request->file('photo')->extension();
-            $final_name = 'author_photo_'.$now.'.'.$ext;
-            $request->file('photo')->move(public_path('uploads/'),$final_name);
+            $final_name = 'author_photo_' . $now . '.' . $ext;
+            $request->file('photo')->move(public_path('uploads/'), $final_name);
             $author->photo = $final_name;
         }
-        
+
         $author->name = $request->name;
         $author->email = $request->email;
         $author->password = Hash::make($request->password);
@@ -55,27 +55,26 @@ class AdminAuthorController extends Controller
         // Send email
         $subject = 'Your account is created to the website';
         $message = 'Hi, your account is created successfully and now you can login to our system from the front end login page. Please go to this link: <br><br>';
-        $message .= '<a href="'.route('login').'">';
+        $message .= '<a href="' . route('login') . '">';
         $message .= 'Click on this link';
         $message .= '</a>';
         $message .= '<br><br>Please see your password here and after login, change that immediately:<br>';
         $message .= $request->password;
 
-        \Mail::to($request->email)->send(new Websitemail($subject,$message));
+        \Mail::to($request->email)->send(new Websitemail($subject, $message));
 
-        return redirect()->route('admin_author_show')->with('success','Author account is created successfully');
-
+        return redirect()->route('admin_author_show')->with('success', 'Author account is created successfully');
     }
 
     public function edit($id)
     {
-        $author_data = Author::where('id',$id)->first();
+        $author_data = Author::where('id', $id)->first();
         return view('admin.author_edit', compact('author_data'));
     }
 
-    public function update(Request $request,$id) 
+    public function update(Request $request, $id)
     {
-        $author = Author::where('id',$id)->first();
+        $author = Author::where('id', $id)->first();
 
         $request->validate([
             'name' => 'required',
@@ -86,30 +85,30 @@ class AdminAuthorController extends Controller
             ]
         ]);
 
-        if($request->password!='') {
+        if ($request->password != '') {
             $request->validate([
                 'password' => 'required',
-                'retype_password' => 'required|same:password'
+                // 'retype_password' => 'required|same:password'
             ]);
             $author->password = Hash::make($request->password);
         }
 
-        if($request->hasFile('photo')) {
+        if ($request->hasFile('photo')) {
             $request->validate([
                 'photo' => 'image|mimes:jpg,jpeg,png,gif'
             ]);
 
-            if ($author->photo && file_exists(public_path('uploads/'.$author->photo)) && is_file(public_path('uploads/'.$author->photo))) {
-            unlink(public_path('uploads/'.$author->photo));
+            if ($author->photo && file_exists(public_path('uploads/' . $author->photo)) && is_file(public_path('uploads/' . $author->photo))) {
+                unlink(public_path('uploads/' . $author->photo));
             }
 
             $now = time();
             $ext = $request->file('photo')->extension();
-            $final_name = 'author_photo_'.$now.'.'.$ext;
-            $request->file('photo')->move(public_path('uploads/'),$final_name);
+            $final_name = 'author_photo_' . $now . '.' . $ext;
+            $request->file('photo')->move(public_path('uploads/'), $final_name);
             $author->photo = $final_name;
         }
-        
+
         $author->name = $request->name;
         $author->email = $request->email;
         $author->update();
@@ -119,13 +118,12 @@ class AdminAuthorController extends Controller
 
     public function delete($id)
     {
-        $author = Author::where('id',$id)->first();
-        if($author->photo != NULL) {
-            unlink(public_path('uploads/'.$author->photo));
+        $author = Author::where('id', $id)->first();
+        if ($author->photo != NULL) {
+            unlink(public_path('uploads/' . $author->photo));
         }
         $author->delete();
 
         return redirect()->route('admin_author_show')->with('success', 'Author is deleted successfully.');
-
     }
 }
